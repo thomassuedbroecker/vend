8e9f26a276c4c0430ef1ff6b9ae005bf44b811ee
thomassuedbroecker <thomas.suedbroecker@de.ibm.com>
2022-07-21 11:15:27 +0200

    small change in configuration

--- build-and-push-container-quay.sh
+++ build-and-push-container-quay.sh
@@ -5,7 +5,8 @@ export ROOT_PATH=$(pwd)
 export IMAGE_NAME=vend-simple:v1
 export URL=quay.io
 export REPOSITORY=tsuedbroecker
+export CONTAINER_ENGINE=podman
 
-docker login quay.io
-docker build -t "$URL/$REPOSITORY/$IMAGE_NAME" -f Dockerfile .
-docker push "$URL/$REPOSITORY/$IMAGE_NAME"
+$CONTAINER_ENGINE login quay.io
+$CONTAINER_ENGINE build -t "$URL/$REPOSITORY/$IMAGE_NAME" -f Dockerfile .
+$CONTAINER_ENGINE push "$URL/$REPOSITORY/$IMAGE_NAME"
